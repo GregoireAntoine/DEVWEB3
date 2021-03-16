7d71a7d8be2bab41b74966bca0d9d694ccdec894
Igor Vandervelden <34266875+Gri097@users.noreply.github.com>
2021-03-16 18:46:11 +0100

    routes product

--- backend/app.js
+++ backend/app.js
@@ -1,73 +1,19 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const mariadb = require('mariadb');
-const pool = mariadb.createPool({
-    host: 'localhost',
-    user:'root',
-    password: '5245',
-    connectionLimit: 5
-});
-pool.getConnection()
-    .then(conn => {
-
-        conn.query("SELECT 1 as val")
-            .then((rows) => {
-                console.log(rows); //[ {val: 1}, meta: ... ]
-                //Table must have been created before
-                // " CREATE TABLE myTable (id int, val varchar(255)) "
-                return conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
-            })
-            .then((res) => {
-                console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
-                conn.end();
-            })
-            .catch(err => {
-                //handle error
-                console.log(err);
-                conn.end();
-            })
-
-    }).catch(err => {
-    //not connected
-});
-
 const app = express();
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-});
-
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.post('/api/stuff', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-        message: 'Objet créé !'
-    });
-});
+const seq = require("./config/config");
+seq.sequelize.sync()
+    .then(() => console.log("--\nDatabase synchronized\n--"))
+    .catch((error) => console.log("An error occurred while Synchronization.\n", error));
 
-app.use('/api/stuff', (req, res, next) => {
-    const stuff = [
-        {
-            _id: 'oeihfzeoi',
-            title: 'Mon premier objet',
-            description: 'Les infos de mon premier objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 4900,
-            userId: 'qsomihvqios',
-        },
-        {
-            _id: 'oeihfzeomoihi',
-            title: 'Mon deuxième objet',
-            description: 'Les infos de mon deuxième objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 2900,
-            userId: 'qsomihvqios',
-        },
-    ];
-    res.status(200).json(stuff);
-});
+const routeProducts = require("./routes/routeProducts");
+
+app.use("/products", routeProducts);
+
+app.listen('3000');
+
 module.exports = app;
