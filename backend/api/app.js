41d1c2bd1b7df31d095001912f412ed0400fd391
Igor Vandervelden <34266875+Gri097@users.noreply.github.com>
2021-03-23 15:45:35 +0100

    set require

--- backend/api/app.js
+++ backend/api/app.js
@@ -1,17 +1,36 @@
 const express = require('express');
 const app = express();
 
+require('dotenv').config();
+const path = require('path');
+const session = require('express-session');
+
+///////////////// Passport ///////////////////////////
+const passport = require('passport');
+app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+const modelUsers = require('./models/modelUsers');
+require('./config/passport.js')(passport, modelUsers);
+module.exports = passport;
+//////////////////////////////////////////////////////
+
+///////////////// Body-parser ////////////////////////
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+///////////////// SYNC DB ////////////////////////////
 const seq = require("./config/config");
 seq.sequelize.sync()
     .then(() => console.log("--\nDatabase synchronized\n--"))
     .catch((error) => console.log("An error occurred while Synchronization.\n", error));
 
+///////////////// Routes /////////////////////////////
 const routeProducts = require("./routes/routeProducts");
+const routeUsers = require("./routes/routeUsers");
 
+app.use("/users", routeUsers);
 app.use("/products", routeProducts);
 
 app.listen('3000');
