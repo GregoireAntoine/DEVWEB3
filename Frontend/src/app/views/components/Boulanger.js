150a5468d58d1a5481599972a0e5813904691992
GregoireAntoine <60960340+GregoireAntoine@users.noreply.github.com>
2021-08-22 19:48:24 +0200

    page du boulanger

---
+++ Frontend/src/app/views/components/Boulanger.js
@@ -0,0 +1,18 @@
+import React, { Fragment, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux'
+import { Link } from "react-router-dom";
+import "../../styles/App.css";
+import { updateCart, removeFromCart } from  '../../lib/actions'
+
+
+  export const Acceuil = props => {
+   
+  
+    return (
+    
+      <input
+      type="checkbox"
+     />
+  
+    );
+}
